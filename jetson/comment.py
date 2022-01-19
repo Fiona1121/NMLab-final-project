a03be6dd4a3db1e6c1c51feea8859d5b54728097
johnny <sniperTHEJ@gmail.com>
2022-01-19 03:38:56 -0800

    Implemented scrolling text

--- jetson/comment.py
+++ jetson/comment.py
@@ -1,29 +1,47 @@
 import requests
 from lcd_control import lcd
+import os
 
 api_keys = "AIzaSyB15-OwpB5w2-uvqboibJwf1Lab3t90zz0"
-stream_id = "_coTdkGk7GE"
-
+stream_id = "0BH1rXp1_sY"
+channel_id = "UCmDybKWIZ1l7Q-p3qOPgVfg"
 
 class comment_Publisher:
     def __init__(self):
-        self.comment_count, self.comments = self.get_liveChat_comment()
+        try:
+            self.comment_count, self.comments = self.get_liveChat_comment()
+        except: 
+            self.comment_count = 0
+            self.comments = []
 
-    def get_chatId(self, stream_id):
+    def get_streamId(self):
+        res =requests.get("https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" + channel_id + "&liveBroadcastContent=live&key=" + api_keys)
+        if res.status_code == 200:
+            return res.json()['items'][0]
+        else:
+            return stream_id
+
+    def get_chatId(self):
         res = requests.get("https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&key=" + api_keys + "&id=" + stream_id)
-        return res.json()['items'][0]['liveStreamingDetails']['activeLiveChatId']
+        try :
+            return res.json()['items'][0]['liveStreamingDetails']['activeLiveChatId']
+        except :
+            return "0"
 
     def get_liveChat_comment(self):
-        chatId = self.get_chatId(stream_id)
+        chatId = self.get_chatId()
         res = requests.get("https://www.googleapis.com/youtube/v3/liveChat/messages?part=id%2C%20snippet&key=" + api_keys + "&liveChatId=" + chatId)
         res = res.json()
-        comment_count = res['pageInfo']['totalResults']
-        comments = []
-        for i in res['items']:
-            username_res = requests.get("https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails&id=" + i['snippet']['authorChannelId'] + "&key=" + api_keys).json()
-            username = username_res['items'][0]['snippet']['title']
-            comments.append((username, i['snippet']['displayMessage']))
-        return (comment_count, comments)
+        try :
+            comment_count = res['pageInfo']['totalResults']
+            comments = []
+            for i in res['items']:
+                username_res = requests.get("https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails&id=" + i['snippet']['authorChannelId'] + "&key=" + api_keys).json()
+                username = username_res['items'][0]['snippet']['title']
+                comments.append((username, i['snippet']['displayMessage']))
+            return (comment_count, comments)
+        except :
+            return (self.comment_count, self.comments)
         
 
     
--- jetson/lcd_control.py
+++ jetson/lcd_control.py
@@ -1,3 +1,6 @@
+# !/usr/bin/python
+# -*-coding:utf-8 -*-
+
 import time,sys
 
 import smbus
@@ -37,7 +40,7 @@ class lcd :
         time.sleep(.05)
         count = 0
         row = 0
-        for c in text:
+        for i, c in enumerate(text):
             if c == '\n' or count == 16:
                 count = 0
                 row += 1
@@ -47,6 +50,34 @@ class lcd :
                 if c == '\n':
                     continue
             count += 1
+            self.bus.write_byte_data(self.DISPLAY_TEXT_ADDR,0x40,ord(c))
+
+    def setText_top(self, text):
+        self.textCommand(0x02) # return home
+        time.sleep(.05)
+        self.textCommand(0x08 | 0x04) # display on, no cursor
+        self.textCommand(0x28) # 2 lines
+        time.sleep(.05)
+        count = 0
+        row = 0
+        for i, c in enumerate(text):
+            if c == '\n' or count == 16:
+                break
+            self.bus.write_byte_data(self.DISPLAY_TEXT_ADDR,0x40,ord(c))
+    
+    def setText_bottom(self, text):
+        self.textCommand(0x02) # return home
+        time.sleep(.05)
+        self.textCommand(0x08 | 0x04) # display on, no cursor
+        self.textCommand(0x28) # 2 lines
+        time.sleep(.05)
+        self.textCommand(0xc0) # skip
+        count = 0
+        row = 0
+        for i, c in enumerate(text):
+            if c == '\n' or count == 16:
+                self.textCommand(0x02)
+                break
             self.bus.write_byte_data(self.DISPLAY_TEXT_ADDR,0x40,ord(c))
  
     #Update the display without erasing the display
@@ -71,6 +102,20 @@ class lcd :
                     continue
             count += 1
             self.bus.write_byte_data(self.DISPLAY_TEXT_ADDR,0x40,ord(c))
+    
+    def clean_text(self, text):
+        lowercase = 'abcdefghijklmnopqrstuvwxyz'
+        uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        digits = '0123456789'
+        punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+        whitespace = ' \t\n\r\x0b\x0c'
+        allowed = [lowercase, uppercase, digits, punctuation, whitespace]
+        cleaned_text = ""
+        for c in text :
+            for case in allowed :
+                if c in case:
+                    cleaned_text = cleaned_text + c
+        return cleaned_text
 
 
 # example code
@@ -78,7 +123,8 @@ if __name__=="__main__":
     lcd_controller = lcd()
     # lcd_controller.setText("Fuck world\nThis is an LCD testeasfwaawf\nawfwafaw\nafgewfa\n")
     lcd_controller.setRGB(255,255,255)
-    lcd_controller.setText("BTC:43000.12\nJohnnyChen:This comment is too damn long")
+    #lcd_controller.setText("BTC:43000.12\nJohnnyChen:This comment is too damn long")
+    #lcd_controller.setText("\n")
     # time.sleep(2)
     # for c in range(0,5):
     #     lcd_controller.setText_norefresh("Going to sleep in {}...".format(str(c)))
@@ -86,3 +132,26 @@ if __name__=="__main__":
     #     time.sleep(0.1)
     # lcd_controller.setRGB(0,255,0)
     #
+    framebuffer = ['Hello!', '',]
+    long_string = "BTC:43000.12JohnnyChen:This comment is too damn long"
+    """for i in range(len(long_string) - 16 + 1):
+        framebuffer[1] = long_string[i:i+16]
+        lcd_controller.setText_norefresh(framebuffer[0] + "\n" + framebuffer[1])
+        time.sleep(0.05)"""
+    lcd_controller.setText("BTC:43000.12\nJohnnyChen:This comment is too damn long")
+    lcd_controller.setText_top(" "*16)
+    lcd_controller.setText_top("helleeeeee")
+    lcd_controller.setText_bottom(" "*16)
+    lcd_controller.setText_bottom("2222222222222")
+    lcd_controller.setText_top(" "*16)
+    lcd_controller.setText_top("h3e")
+    lcd_controller.setText_bottom(" "*16)
+    lcd_controller.setText_bottom("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    lower_text = "This does not work as expected at least on python 3.7"
+    #lower_text = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    framebuffer = ["", lower_text]
+    #lcd_controller.setText_bottom(lower_text)
+    for i in range(len(lower_text) - 16 + 1):
+        framebuffer[1] = lower_text[i:i+16]
+        lcd_controller.setText_bottom(framebuffer[1])
+        time.sleep(0.2)
